cd7c3a43dee7c753d177d954890d624d8655c6d6
lahaina <nattolecats@gmail.com>
2023-10-09 02:15:05 +0900

    sunfish: Prebuilt kernel image setting up * Because ton of errors now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,4 +6,7 @@ if ! history | tail -n 1 | grep -q "sunfish"; then return; fi
 # Give it officiality
 export EVO_BUILD_TYPE=OFFICIAL
 
+# Prebuilt kernel setup
+git clone https://android.googlesource.com/device/google/sunfish-kernel device/google/sunfish-kernel --depth 1
+
 return
