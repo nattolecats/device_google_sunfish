d57f4e37c97314c1fbe1a33d5195707e4ab8f59e
PixelBoot <bootingpixels@gmail.com>
2023-10-18 19:48:33 +0200

    Revert "SystemUIGoogle -> SystemUI"
    This reverts commit bea7452a6bb31049691bf6e5f7e0f715dd56d910.

--- device.mk
+++ device.mk
@@ -951,7 +951,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SystemUIGoogle
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
